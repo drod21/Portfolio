04f8cc8471962fc6be4b9d9ef7499d80bd6bbc4f
Derek Rodriguez <drod2169@gmail.com>
2018-08-07 07:05:28 -0400

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Home } from './views/home';
+import { About } from './views/about';
+import { Contact } from './views/contact';
+import { Portfolio } from './views/portfolio';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path='/portfolio' component={Portfolio} />
+        </Router>
       </div>
     );
   }
