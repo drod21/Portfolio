b89f87abc10e7358186325790709217c041a480c
Derek Rodriguez <drod2169@gmail.com>
2018-08-10 07:04:09 -0400

    Remove console warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './views/home';
 import About from './views/about';
 import Contact from './views/contact';
